3697c04de2ef96b3c5a4010098f9f3bbefe920b8
pringle97 <malia.pringle@gmail.com>
2021-12-21 19:58:45 -0800

    category routes

--- routes/category-routes.js
+++ routes/category-routes.js
@@ -6,25 +6,33 @@ const { Category, Product } = require('../models')
 router.get('/categories', (req, res) => {
   // find all categories
   // be sure to include its associated Products
-  // const categories = await Category.findAll({ include: [Product] })
-  // res.json(categories)
+  const categories = await Category.findAll({ include: [Product] })
+  res.json(categories)
 })
 
 router.get('/categories/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  const categories = await Category.findOne({ where: { id: req.params.id }, include: [Product] })
+  res.json(categories)
 })
 
 router.post('/categories', (req, res) => {
   // create a new category
+  const categories = await Category.create(req.body)
+  res.sendStatus(200)
 })
 
 router.put('/categories/:id', (req, res) => {
   // update a category by its `id` value
+  const categories = await Category.update(req.body, { where: { id } })
+  res.sendStatus(200)
 })
 
 router.delete('/categories/:id', (req, res) => {
   // delete a category by its `id` value
+  const categories = await Category.destroy({ where: { id: req.params.id } })
+  res.sendStatus(200)
 })
 
 module.exports = router
