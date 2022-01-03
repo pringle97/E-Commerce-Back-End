2183f5cb3199399b8f0e353ef1617fa633f20724
pringle97 <malia.pringle@gmail.com>
2022-01-02 23:09:35 -0800

    fixed producttag

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,19 +7,19 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
     product_id: {
       type: DataTypes.INTEGER,
-      references: {
-        model: 'product',
-        key: 'id'
-      }
+      references: { model: "product", key: "id" },
     },
     tag_id: {
       type: DataTypes.INTEGER,
-      references: {
-        model: 'tag',
-        key: 'id'
-      }
+      references: { model: "tag", key: "id" },
     }
   },
   {
